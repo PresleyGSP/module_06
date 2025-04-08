a711bb8570b0e107a5c9e93a5089c59335bba07d
Presley McFarlane-Goolcharan <pmcfarlanegoolcharan@rrc.ca>
2025-04-07 20:35:03 -0500

    Third commit: finished step three.

--- README.md
+++ README.md
@@ -8,3 +8,13 @@ and will be to perform methods on those data items.
 ## Author
 
 Presley McFarlane-Goolcharan
+
+## Code Modification 1
+
+- Defined a Class called "MortgageRate" and stored fixed and variable rate values in the class.
+- Defined a Class called "PaymentFrequency" and stored frequency values in the class.
+
+## Code Modification 2
+
+- Defined a Class called "Mortgage"  with private attributes and implemented exception handling to validate input.
+
--- mortgage/mortgage.py
+++ mortgage/mortgage.py
@@ -1,9 +1,43 @@
 """
 Description: A class meant to manage Mortgage options.
-Author: {Student Name}
-Date: {Date}
+Author: Presley McFarlane-Goolcharan
+Date: April 7th 2025
 Usage: Create an instance of the Mortgage class to manage mortgage records and 
 calculate payments.
 """
+from mortgage.pixell_lookup import MortgageRate, PaymentFrequency, VALID_AMORTIZATION
+
+
+class Mortgage:
+    """This class will make use of mortgage rates, payment frequency values and amortization."""
+
+    def __init__(self, loan_amount: float, rate: str, frequency: str, amortization: int):
+        # Validate Loan Amount
+        if loan_amount <= 0:
+            raise ValueError("Loan Amount must be positive.")
+        self.__loan_amount = loan_amount
+
+        #Validating Rate
+        try:
+                self.__rate = MortgageRate[rate]
+        except Exception as e:
+                raise ValueError("Rate provided is invalid.")
+        
+        # Validate Frequency
+        try:
+                self.__frequency = PaymentFrequency[frequency]
+        except Exception as e:
+                raise ValueError("Frequency provided is invalid.")
+        
+        # Validate Amortization
+        if amortization not in VALID_AMORTIZATION:
+            raise ValueError("Amortization provided is invalid.")
+        self.__amortization = amortization
+        
+        
+
+
+
+
 
 
